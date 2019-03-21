7e4ad40b35e1151e79a58842c0d142586b6f6fb4
周昊 <zhouhoa8@xiaomi.com>
2019-03-21 13:44:50 +0800

    feat: 添加Less支持

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import './test2.less';
 import { Button } from 'antd';
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Button type="danger">fff</Button>
+        <p className="test">123</p>
       </div>
     );
   }
